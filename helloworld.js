029213fd49365f7f7ba9b1c5e80498bd5e449d44
zenbox <michael@zenbox.de>
2019-04-01 09:23:03 +0200

    init

--- helloworld.js
+++
@@ -1,8 +0,0 @@
-function hw() {
-  console.log('Hello World!');
-  return true;
-}
-
-setInterval(function () {
-  hw();
-}, 1000);
--- typescript/dist/hello-world.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * a first Typescript Example
- */
-var sayHelloTo = function (target) {
-    return 'Hello' + target;
-};
-console.log(sayHelloTo('Welt'));
