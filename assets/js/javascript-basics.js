c6026fb9c0c3405bd77835b54f1ba182b76a6080
zenbox <michael@zenbox.de>
2019-05-20 15:21:14 +0200

    Create javascript-basics.js

---
+++ assets/js/javascript-basics.js
@@ -0,0 +1,19 @@
+/* global console, window, document */
+/**
+ * Javascript basics
+ *
+ * @package Webapplication
+ * @author Michael [michael@zenbox.de]
+ * @since 2019/05/20
+ * @version v1.0.0
+ * @copyright (c) 2018 Michael Reichart, Cologne
+ * @license MIT License [https://opensource.org/licenses/MIT]
+ */
+
+var a = 42;
+var b = 108;
+
+if (typeof a === 'number' && typeof b === 'number') {
+  console.log(a + b);
+}
+console.log(typeof a);
