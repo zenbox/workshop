4d5566baf716d9e4ea156d064adf11ccec57564c
zenbox <michael@zenbox.de>
2019-05-23 15:48:08 +0200

    notification refactor

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -16,61 +16,81 @@ jQuery(document)
     // - - - - - - - - - -
     // DECLARATION
     var
+      // variables
       container = undefined,
-      messages = [{
-        message: 'Aenean lacinia bibendum nulla sed consectetur.',
-        category: 'warning',
-        blendingTime: 0,
-        messageTime: 1000
-      }, {
-        message: 'Maecenas sed diam eget risus varius blandit sit amet non magna.',
-        category: 'strange',
-        blendingTime: 0,
-        messageTime: 500
-      }, {
-        message: 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.',
-        category: 'success',
-        blendingTime: 0,
-        messageTime: 500
-      }, {
-        message: 'Nullam id dolor id nibh ultricies vehicula ut id elit.',
-        category: 'default',
-        blendingTime: 0,
-        messageTime: 500
-      }, {
-        message: 'Cras mattis consectetur purus sit amet fermentum.',
-        category: 'success',
-        blendingTime: 0,
-        messageTime: 500
-      }, {
-        message: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.',
-        category: 'warning',
-        blendingTime: 0,
-        messageTime: 500
-      }, {
-        message: 'Sed posuere consectetur est at lobortis.',
-        category: 'success',
-        blendingTime: 0,
-        messageTime: 500
-      }, {
-        message: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.',
-        category: 'error',
-        blendingTime: 0,
-        messageTime: 500
-      }, {
-        message: 'Sed posuere consectetur est at lobortis.'
-      }],
+      messages = undefined,
+      // method declarations
       main = function () {},
       selectContainer = function () {},
       removeContainer = function () {},
       buildContainer = function () {},
-      buildMessage = function () {};
+      buildMessage = function () {},
+      getMessage = function () {};
+
+    messages = [{
+      message: 'Aenean lacinia bibendum nulla sed consectetur.',
+      category: 'warning',
+      blendingTime: 0,
+      messageTime: 1000
+    }, {
+      message: 'Maecenas sed diam eget risus varius blandit sit amet non magna.',
+      category: 'strange',
+      blendingTime: 0,
+      messageTime: 500
+    }, {
+      message: 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.',
+      category: 'success',
+      blendingTime: 0,
+      messageTime: 500
+    }, {
+      message: 'Nullam id dolor id nibh ultricies vehicula ut id elit.',
+      category: 'default',
+      blendingTime: 0,
+      messageTime: 500
+    }, {
+      message: 'Cras mattis consectetur purus sit amet fermentum.',
+      category: 'success',
+      blendingTime: 0,
+      messageTime: 500
+    }, {
+      message: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.',
+      category: 'warning',
+      blendingTime: 0,
+      messageTime: 500
+    }, {
+      message: 'Sed posuere consectetur est at lobortis.',
+      category: 'success',
+      blendingTime: 0,
+      messageTime: 500
+    }, {
+      message: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.',
+      category: 'error',
+      blendingTime: 0,
+      messageTime: 500
+    }, {
+      message: 'Sed posuere consectetur est at lobortis.'
+    }];
 
     // METHODS
+    /**
+     * selects the messages container
+     * @return {boolean} if false
+     */
     selectContainer = function () {
+      // ! container is global
       container = jQuery('#notifications');
+
+      if (container === undefined) return false;
+      if (container.length === 0) return false;
+
+      return true;
     };
 
+    /**
+     * builds the messages container
+     * @param  {Function} fn callback for selectContainer
+     * @return {boolean}     if false
+     */
     buildContainer = function (fn) {
       var _callback = fn || undefined;
 
@@ -83,23 +103,43 @@ jQuery(document)
       _callback();
     };
 
+    /**
+     * removes the messages container
+     * @return {boolean} if false
+     */
     removeContainer = function () {
       if (container === undefined) return false;
       container.remove();
     };
 
+    /**
+     * building notification messages
+     * @param  {object} m contains message, time and category values
+     * @return {boolean}  if false
+     */
     buildMessage = function (m) {
+      console.log(m);
+
       var
-        _message = m.message || undefined,
-        _messageTime = m._messageTime || 2000,
-        _blendingTime = m.blendingTime || 500,
-        _category = m.category || 'default';
+        _m = m || undefined,
+        _message = undefined,
+        _messageTime = undefined,
+        _blendingTime = undefined,
+        _category = undefined;
 
-      if (typeof m !== 'object') return false;
-      if (_message === undefined) return false;
-      if (typeof _message !== 'string') return false;
-
+      if (_m === undefined) return false;
+      if (typeof _m === 'string') {
+        _m = {
+          message: _m
+        };
+      }
+      if (typeof _m !== 'object') return false;
       if (container === undefined) return false;
+
+      _message = _m.message || 'no message available';
+      _messageTime = _m.messageTime || 2000;
+      _blendingTime = _m.blendingTime || 500;
+      _category = _m.category || 'default';
 
       container
         .append('<div>')
@@ -117,6 +157,15 @@ jQuery(document)
     }
 
     /**
+     * grabs a random message
+     * @return {object} the message object
+     */
+    getMessage = function () {
+      var _randomNumber = Math.floor(Math.random() * (messages.length)),
+        _message = messages[_randomNumber];
+      return _message;
+    };
+    /**
      * the main call of this app
      * @param  {mixed} args [description]
      * @return {boolean}    [description]
@@ -130,19 +179,13 @@ jQuery(document)
     jQuery(function () {
       main();
       buildContainer(selectContainer);
-      buildMessage({
-        message: 'huddel!! huddel!! huddel!! huddel!! huddel!!'
-      });
+      buildMessage('hello world');
       // removeContainer();
     })
     // EVENT CONTROL
     jQuery('h1')
       .on('click', function () {
-        var
-          _randomNumber = Math.floor(Math.random() * (messages.length)),
-          _message = messages[_randomNumber];
-
-        buildMessage(_message);
+        buildMessage(getMessage());
       });
     // - - - - - - - - - -
   });
