89a2160f51e0163a9d2a644da0fdb08aec249209
zenbox <michael@zenbox.de>
2019-05-24 11:52:44 +0200

    Update add.js

--- assets/js/add.js
+++ assets/js/add.js
@@ -3,19 +3,14 @@
  */
 
 function add(a, b) {
-  if (typeof a) === 'string'
-  return false;
-  if (typeof b) === 'string'
-  return false;
+
+  if (typeof a = 'string' && parseInt(a) == a);
+  if (typeof b = 'string' && parseInt(b) == b);
 
   return a + b;
 }
 
-
-
-
 assert.ok(add(3, 4) === 7); // true
 assert.ok(add(3, "4") === false); // true
-
 
 //assert.ok(add(4, 4) == 7); // false
