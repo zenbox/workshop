39eb24d7e0cb48933fd2c7715cd75bcf413add0b
zenbox <michael@zenbox.de>
2019-06-05 10:16:10 +0200

    Update events.js

--- assets/js/events.js
+++ assets/js/events.js
@@ -11,52 +11,88 @@
  */
 
 jQuery(function ($) {
-  'use strict';
-  // - - - - - - - - - -
-  // DECLARATION
-  // - - - - - - - - - -
-  let init = undefined;
+      'use strict';
+      // - - - - - - - - - -
+      // DECLARATION
+      // - - - - - - - - - -
+      let init = undefined;
 
-  // - - - - - - - - - -
-  // METHODS
-  // - - - - - - - - - -
-  init = function () {};
+      // - - - - - - - - - -
+      // METHODS
+      // - - - - - - - - - -
+      init = function () {};
 
-  // - - - - - - - - - -
-  // EVENT HANDLER METHODS
-  // - - - - - - - - - -
-  function onAnchorClick(e) {
-    var _event = e || window.event;
+      // - - - - - - - - - -
+      // EVENT HANDLER METHODS
+      // - - - - - - - - - -
+      function onAnchorClick(e) {
+        var _event = e || window.event;
 
-    //! early exit
-    if (!_event) return false;
+        //! early exit
+        if (!_event) return false;
 
-    //! cancel the browsers job
-    _event.preventDefault();
+        //! cancel the browsers job
+        _event.preventDefault();
 
-    //! do the job
-    console.dir(_event);
-    console.dir(window.event);
-    console.log('click on anchor!');
+        //! do the job
+        console.log('click on anchor!');
 
-    console.log(_event.target);
-    console.log(_event.type);
+        console.log(_event.target);
+        console.log(_event.type); // click!
+        console.log(_event.which); // 1 (left)
+      }
 
+      function onContextmenu(e) {
+        let _event = e || window.event;
 
+        //! early exit
+        if (!_event) return false;
 
-  }
+        //! cancel the browsers job
+        _event.preventDefault();
 
-  // - - - - - - - - - -
-  // CONTROL
-  // - - - - - - - - - -
-  $(function () {
-    init();
-    $('a[href]')
-      .on('click', onAnchorClick);
-    $('a[href]')
-      .off('click', onAnchorClick);
-    $('a[href]')
-      .one('click', onAnchorClick);
-  });
-  // - - - - - - - - - -
-}(jQuery));
+        // ! do the job
+        console.log(_event.target);
+        console.log(_event.type);
+        console.log(_event.which);
+      }
+      /**
+       *
+       * @param  {object} e  event object
+       * @return {boolean}   wether the function did the job or not
+       */
+      function onFormLoginSubmit(e) {
+        let _event = e || window.event;
+
+        //! early exit
+        if (!_event) return false;
+
+        //! cancel the browsers job
+        _event.preventDefault();
+
+        // ! do the job
+
+      }
+
+      // - - - - - - - - - -
+      // CONTROL
+      // - - - - - - - - - -
+      $(function () {
+        init();
+        // set an event listener, delegate events!
+        $('nav > ul')
+          // on: type, [delegation,] [data,] callback
+          .on('click', 'a[href]', onAnchorClick);
+
+        $('body')
+          .on('contextmenu', onContextmenu);
+
+        $('form#form-login')
+          .on('submit', onFormLoginSubmit());
+
+        // // delete event listeners
+        // $('nav > ul')
+        //   .off(click ');
+        //   });
+        // - - - - - - - - - -
+      }(jQuery));
