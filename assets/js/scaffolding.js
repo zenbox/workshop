7967b217d8f9b945e850d9fc1263eb11a326c32b
zenbox <michael@zenbox.de>
2019-06-24 16:16:57 +0200

    Update scaffolding.js

--- assets/js/scaffolding.js
+++ assets/js/scaffolding.js
@@ -14,12 +14,39 @@
   'use strict';
   // - - - - - - - - - -
   // DECLARATION
-  let main = undefined;
+  // NEVER IN YOUR LIFE!!
+  let
+    main,
+    onFocusInputEmail,
+    onBlurInputEmail,
+    inputEmail = document.querySelector('#login-email'),
+    inputPassword = document.querySelector('#login-password');
 
   // METHODS
-  main = function () {};
+  main = function () {
+    // EVENT CONTROL
+    inputEmail.addEventListener('focus', onFocusInputEmail);
+    inputEmail.addEventListener('blur', onBlurInputEmail);
+  };
+
+  // EVENT HANDLER METHODS
+  /**
+   * @param {Event} event the event object
+   * @return none
+   */
+  onFocusInputEmail = function (event) {
+    console.dir(event);
+  };
+  /**
+   * @param {Event} event the event object
+   * @return none
+   */
+  onBlurInputEmail = function (event) {
+    console.log(event.target);
+  };
 
   // CONTROL
   window.addEventListener('load', main);
+
   // - - - - - - - - - -
 }());
