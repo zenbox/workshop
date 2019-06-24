c3e8fa2d3cef4ff81a52d1eb65d543f1b254ee0c
zenbox <michael@zenbox.de>
2019-06-24 16:16:53 +0200

    Create events.js

---
+++ assets/js/events.js
@@ -0,0 +1,33 @@
+/* global console, window, document */
+/**
+ * Event Handling
+ *
+ * @package Webapplication
+ * @author Michael [michael@zenbox.de]
+ * @since 2019/06/24
+ * @version v1.0.0
+ * @copyright (c) 2019 Michael Reichart, Cologne
+ * @license MIT License [https://opensource.org/licenses/MIT]
+ */
+
+!(function () {
+  'use strict';
+  // - - - - - - - - - -
+  // DECLARATION
+  let
+    main = undefined,
+  ;
+
+
+  // METHODS
+  main = function () {
+    let collection = document.querySelectorAll('a[href]');
+
+    // Event listener
+
+  };
+
+  // CONTROL
+  window.addEventListener('load', main);
+  // - - - - - - - - - -
+}());
