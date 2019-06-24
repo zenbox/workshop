34d3b171ba56738c26bdc17cd351032e7f384bd3
zenbox <michael@zenbox.de>
2019-06-24 13:49:42 +0200

    Adding scaffolding

---
+++ assets/js/scaffolding.js
@@ -0,0 +1,25 @@
+/* global console, window, document */
+/**
+ * A Javascript Architecture
+ *
+ * @package Webapplication
+ * @author Michael [michael@zenbox.de]
+ * @since 2019/06/24
+ * @version v1.0.0
+ * @copyright (c) 2019 Michael Reichart, Cologne
+ * @license MIT License [https://opensource.org/licenses/MIT]
+ */
+
+!(function () {
+  'use strict';
+  // - - - - - - - - - -
+  // DECLARATION
+  let main = undefined;
+
+  // METHODS
+  main = function () {};
+
+  // CONTROL
+  window.addEventListener('load', main);
+  // - - - - - - - - - -
+}());
