40a9593f479520133bf778144fbbcac84f19f781
zenbox <michael@zenbox.de>
2019-06-26 07:45:56 +0200

    Create login.js

---
+++ assets/js/login.js
@@ -0,0 +1,76 @@
+/* global console, window, document */
+/**
+ * Login
+ *
+ * @package Webapplication
+ * @author Michael [michael@zenbox.de]
+ * @since 2019/06/25
+ * @version v1.0.0
+ * @copyright (c) 2019 Michael Reichart, Cologne
+ * @license MIT License [https://opensource.org/licenses/MIT]
+ */
+
+!(function () {
+  'use strict';
+  // - - - - - - - - - -
+  // DECLARATION
+  let
+    main = undefined,
+    onLogin = undefined,
+    getLoginCredentials = undefined,
+    submitLoginCredentials = undefined;
+
+  // METHODS
+  /**
+   * @return {Object}
+   */
+  getLoginCredentials = function () {
+
+    let email = document.querySelector('#login-email')
+      .value.trim();
+    let password = document.querySelector('#login-password')
+      .value.trim();
+
+    if (email === '') {
+      throw 'invalid email value';
+    }
+
+    if (password === '') {
+      throw 'invalid password value';
+    }
+
+    return {
+      email: email,
+      password: password
+    };
+  };
+
+  /**
+   *
+   * @param  {Event} event
+   * @return {[type]}
+   */
+  onLogin = function (event) {
+
+    event.preventDefault();
+
+    try {
+      let loginCredentials = getLoginCredentials();
+      console.log(loginCredentials);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  main = function () {
+
+    document.querySelector('form#login')
+      .addEventListener('submit', onLogin);
+
+  };
+
+  // CONTROL
+  window.addEventListener('load', main);
+  // - - - - - - - - - -
+}());
