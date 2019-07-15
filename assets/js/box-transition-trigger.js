32abf747f52dc9931c626744bcfadbcceed7147c
zenbox <michael@zenbox.de>
2019-07-15 08:24:53 +0200

    init Javascript Basics

--- assets/js/box-transition-trigger.js
+++
@@ -1,9 +0,0 @@
-!(function () {
-    'use strict';
-
-    let box = document.querySelector('#box');
-    box.addEventListener('click', function (event) {
-        event.target.classList.toggle('left')
-        event.target.classList.toggle('right')
-    })
-})();
