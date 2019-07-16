6243d7ef1670275b38ed8c8bd8631ae55b952fb8
zenbox <michael@zenbox.de>
2019-07-16 13:57:12 +0200

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+/**
+ * A Main Application
+ * 
+ * @desc Cum sociis natoque penatibus et magnis dis parturient montes, 
+ *       nascetur ridiculus mus. Cum sociis natoque penatibus et magnis dis 
+ *       parturient montes, nascetur ridiculus mus. Etiam porta sem malesuada 
+ *       magna mollis euismod. Duis mollis, est non commodo luctus, 
+ *       nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+ * 
+ * @package ApplicationName
+ * @module Main
+ * @see /other/modules
+ * @author Michael <michael.reichart@gfu.net>
+ * @author Stephan <stephan ...>
+ * @version 1.0.0
+ * @since 1.0.0
+ * @since 2019/07/16
+ * @license MIT <https://opensource.org/licenses/MIT>
+ * @copyright 2019 Michael Reichart
+ */
+
+console.log(window);
+
+!(function () {
+    'use strict';
+
+
+
+})()
+
+
+// Select something from DOM
+document
+    .querySelector('h1')
+    .innerText = 'A New Text';
+
+document
+    .querySelector('p')
+    .innerText = 'A New Text with some more stuff';
