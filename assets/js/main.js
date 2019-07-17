e5dbc6a8fba0921481811a6347cd9a0d2e482f4e
zenbox <michael@zenbox.de>
2019-07-17 16:05:44 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,58 +19,156 @@
  * @copyright 2019 Michael Reichart
  */
 
-!(function() {
-  "use strict";
-  // - - - - - - - - - -
-  // DECLARATION
-  // - - - - -
-  let formLogin = document.querySelector("#form-login"),
-    loginEmail = document.querySelector("#login-email");
+!(function () {
+    "use strict";
+    // - - - - - - - - - -
+    // DECLARATION
+    // - - - - -
+    let formLogin = document.querySelector("#form-login"),
+        loginEmail = document.querySelector("#login-email"),
+        navigationGroups = document.querySelectorAll('nav');
 
-  console.dir(formLogin);
+    // - - - - -
+    // METHODS
+    // - - - - -
+    /** A shorter log function
+     * @param {any} m
+     */
+    function log(m = undefined) {
+        let _m = m;
 
-  // - - - - -
-  // METHODS
-  // - - - - -
-  /**
-   * A shorter log function
-   * @param {any} m
-   */
-  function log(m = undefined) {
-    let _m = m;
+        if (_m === undefined) return false;
 
-    if (_m === undefined) return false;
+        console.log(_m);
+        return true;
+    }
 
-    console.log(_m);
-    return true;
-  }
+    /** Setting text nodes into dom elements
+     * 
+     * @param {string} eln  the new elements name
+     * @param {string} ctx  the appending dom context
+     * @return {boolean}    works or not
+     */
+    function setText(txt, ctx) {
+        let
+            _text = txt || undefined,
+            _context = ctx || undefined,
+            _textNode = undefined;
 
-  /**
-   * The magic event object
-   * @param {Event} event the event facts collection
-   */
-  function onFormLoginSubmit(event) {
-    event.preventDefault();
-    log("form submitted!");
-  }
-  /**
-   * main process
-   */
-  function main() {}
+        console.log('text');
 
-  // - - - - -
-  // MAIN-CONTROL
-  // - - - - -
-  window.addEventListener("load", main);
 
-  // - - - - -
-  // EVENT-CONTROL
-  // - - - - -
-  formLogin.addEventListener("submit", onFormLoginSubmit);
+        if (_text === undefined || _context === undefined) return false;
 
-  // - - - - - - - - - -
+        _textNode = document.createTextNode(_text);
+        document.querySelector(_context).appendChild(_textNode);
+
+        return true;
+    }
+
+    /** setting new dom nodes with attributes
+     * 
+     * @param {string} eln  the new elements name
+     * @param {object} att  the collection of attributes
+     * @param {string} ctx  the appending dom context
+     * @return {boolean}    works or not
+     */
+    function setElement(eln, att, ctx) {
+        let
+            _elementName = eln || undefined,
+            _attributes = att || undefined,
+            _context = ctx || undefined,
+            _node = undefined;
+
+        if (_elementName === undefined || _context === undefined) return false;
+
+        _node = document.createElement(_elementName);
+
+        for (let key in _attributes) {
+            _node.setAttribute(key, _attributes[key]);
+        }
+
+        document.querySelector(_context).appendChild(_node);
+
+        return true;
+    }
+
+    /** The magic event object
+     * @param {Event} event the event facts collection
+     */
+    function onFormLoginSubmit(event) {
+        // shut up, browser!
+        event.preventDefault();
+        log("form submitted!");
+
+    }
+
+    /** Event handler hor nav groups
+     * 
+     * @param {Event} event 
+     */
+    function onAnchorClick(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        switch (event.target.tagName) {
+            case 'A':
+                break;
+            case 'LI':
+                break;
+            default:
+                break
+        }
+    }
+
+    /** Main process
+     * @param none
+     * @return boolean works or not
+     */
+    function $init() {
+        // build a label
+        setElement('label', {
+            id: 'keep-password'
+        }, 'fieldset');
+
+        // build a checkbox
+        setElement('input', {
+            type: 'checkbox',
+            name: 'keep',
+            value: 'yes'
+        }, '#keep-password');
+
+        // build text
+        setText('keep password', '#keep-password');
+
+        return true;
+    }
+
+    // - - - - -
+    // $INIT-CONTROL
+    // - - - - -
+    window.addEventListener("load", $init);
+
+    // - - - - -
+    // EVENT-CONTROL
+    // - - - - -
+    formLogin.addEventListener("submit", onFormLoginSubmit);
+
+    // event delegation,
+    // using querySelectorall
+    for (let i = 0; i < navigationGroups.length; i += 1) {
+        navigationGroups[i].addEventListener('click', onAnchorClick);
+    }
+    // - - - - - - - - - -
 })();
 
 // Select something from DOM
 // document.querySelector("h1").innerText = "A New Text";
 // document.querySelector("p").innerText = "A New Text with some more stuff";
+
+
+
+
+
+
+// $('nav').on('click', 'a[href]', onClick)
