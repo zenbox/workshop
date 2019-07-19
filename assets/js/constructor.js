c7b5ba3aa7fd8bcbe9feae0d8af0346d7a83c832
zenbox <michael@zenbox.de>
2019-07-19 15:38:53 +0200

    Update constructor.js

--- assets/js/constructor.js
+++ assets/js/constructor.js
@@ -14,6 +14,9 @@
      'use strict';
      // - - - - - - - - - -
 
+     const DEBUG = true;
+
+
      // object types in JS
      // [], {}, function () {}, null, undefined, String, Number, Boolean, Infinity, NaN ...
 
@@ -22,34 +25,60 @@
      //! DEPRECATED
      var b = new String('hello world'); // typeof -> object 
 
+     // - - - -
      // old fashioned!
      // Classes -> Prototypes
      var Video = function (vt, l) {
          // private variables
          var a = 42;
-
          // public properties
-         this.title = vt || undefined;
-         this.length = l || undefined;
+         this.title = undefined;
+         this.length = undefined;
 
+         this.init(vt, l);
+
+         //? using the new Video object within?
+         //! probably not a good idea
          Video.prototype.stop = function () {
-             console.log(this.title + ' stopped');
+             if (DEBUG) console.log(this.title + ' stopped');
          }
      };
 
      // methods
+     //! where the wolfpack runs
+     Video.prototype.init = function (vt, l) {
+         this.title = vt || undefined;
+         this.length = l || undefined;
+     }
+
      Video.prototype.play = function () {
          console.log('play ' + this.title);
      }
 
+     // Child class of video
+     var WebVideo = function (vt, l) {
+         this.parent = Video.prototype;
+         this.parent.init(vt, l);
+     };
+
+     WebVideo.prototype = new Video();
+
+
      // instatiate
      var myVideo = new Video('the video title', '30min');
+     var myVideo2 = new Video('the video title', '30min');
+
+     // use the object
      myVideo.play();
+
+     // child class
+     var myWebVideo = new WebVideo('the video title', '200min');
+
+
      console.log(myVideo);
+     console.log(myWebVideo);
 
-
-
-
+     // - - - -
      // OMNIS
      var Component = function () {
          this.title = 'component';
@@ -58,6 +87,7 @@
      var ctrl_net_omnis_myComponent = new Component();
 
 
+     // module pattern, iife pattern, constructor
      ctrl_net_omnis_myComponent.prototype = (function () {
          var PRIVATE
          var ctrl = {};
@@ -66,36 +96,20 @@
 
          return ctrl;
      })();
-
-
-     console.log(ctrl_net_omnis_myComponent);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
      // - - - - - - - - - -
  })()
+
+
+
+
+
+
+ // - - - - - - - - - -
+ let a = 42;
+ (a) => {
+     return a;
+ }
+
+ document.querySelector('body').addEventListener('load', (event) => {
+     return event.target
+ });
