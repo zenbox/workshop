d16968437f07f874d203abc238f7b0e3a9a638f1
zenbox <michael@zenbox.de>
2019-10-09 14:53:48 +0200

    Update interface.js

--- assets/js/interface.js
+++ assets/js/interface.js
@@ -20,21 +20,68 @@
 
     // FUNCTIONS
     function onAnchorClick(event) {
+        // DECLARATION
+        let tagName = event.target.tagName;
+        let url = event.target;
+        let target = event.target.dataset.target;
+
+        // CONTROL
         // stop the browser action
         event.preventDefault();
-        switch (event.target.tagName) {
+
+        switch (tagName) {
             case 'A':
-                console.dir(event.target);
+                loadData(url, target);
                 break;
             default:
                 break;
         }
     }
 
-    // EVENT LISTENER (CONTROL)
-    for (let i = 0; i < myAnchors.length; i += 1) {
-        myAnchors[i].addEventListener('click', onAnchorClick);
+    function loadData(url) {
+        console.log('loading ...');
+
+        // AJAX:
+        let request = new XMLHttpRequest();
+
+        request.addEventListener('readystatechange', function (event) {
+            console.log(request.readyState);
+
+            switch (request.readyState) {
+                default:
+                case 0:
+                    break;
+                case 1:
+                    console.log('request opened.');
+                    break;
+                case 2:
+                    console.log('request sent.');
+                    break;
+                case 3:
+                    console.log('response head + first data.');
+                    break;
+                case 4:
+                    console.dir(request);
+                    console.log('response next data + ok.');
+                    changeContent(request.responseText);
+                    break;
+            }
+
+        });
+
+        // open(http-method, url)
+        request.open('get', url);
+        request.send();
     }
+
+    function changeContent(content) {
+        document.querySelector(contentTargetSelector).innerHTML = content;
+    }
+
+    // EVENT LISTENER (CONTROL)
+    // for (let i = 0; i < myAnchors.length; i += 1) {
+    //     myAnchors[i].addEventListener('click', onAnchorClick);
+    // }
     // Delegated Event
     myNavBar.addEventListener('click', onAnchorClick);
     // - - - - - - - - - -
