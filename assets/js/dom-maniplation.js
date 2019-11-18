5785eed233aac64103df2f36f649da0eff4cce9a
zenbox <michael@zenbox.de>
2019-11-18 13:23:08 +0100

    Update dom-maniplation.js

--- assets/js/dom-maniplation.js
+++ assets/js/dom-maniplation.js
@@ -28,16 +28,74 @@
 
     // better as object with console.dir!
     console.dir(document);
-    document.body.bgColor = "red";
+    document.body.bgColor = "white";
     document.body.style.bgColor = "green";
     // doesn't work:
     // window.document.html.body.h1.innerText ="New Headline"
 
     // use selectors!
     let h1 = document.querySelector('h1');
+    let arr = document.querySelectorAll('h1');
+
+    // using an ID
     h1 = document.querySelector('#my-headline');
     h1 = document.getElementById('my-headline');
 
+    // modify an object
     h1.innerText = "A New Headline";
+
+    // modify an array of objects
+    for (let i = 0; i < arr.length; i += 1) {
+        arr[i].innerText = "A Very New Headline";
+    }
+
+    // see object
+    console.log(h1);
+    console.dir(h1);
+
+
+    // other selector commands
+    document.querySelector('h1');
+    document.querySelectorAll('.teaser');
+
+    document.getElementById('my-headline');
+    document.getElementsByName('p');
+
+    let arrByClasses = document.getElementsByClassName('teaser');
+    console.log(arrByClasses.length);
+
+
+    // creating elements
+    let list = document.createElement('ul');
+
+    // creating text nodes
+    let text = document.createTextNode('hello world!');
+
+    // append elements to the DOM
+    document.body.appendChild(list);
+
+    // set attributes
+    list.setAttribute('class', 'navigation');
+
+    let navArray = [
+        ["Home", "index.html"],
+        ["Work", "work.html"],
+        ["Contact", "contact.html"],
+        ["Imprint", "imprint.html"]
+    ];
+ 
+    for (let i = 0; i < navArray.length; i++) {
+        const element = navArray[i];
+        console.log(element[1]);
+    }
+
+
+
+
+
+
+
+
+
     // - - - - - - - - - -
 }());
