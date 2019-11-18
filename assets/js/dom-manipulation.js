6b93a198a2f47ba24202c557c6f718fbb0a2ff9d
zenbox <michael@zenbox.de>
2019-11-18 15:54:43 +0100

    Update dom-manipulation.js

--- assets/js/dom-manipulation.js
+++ assets/js/dom-manipulation.js
@@ -1,6 +1,6 @@
 /** Dom Manipulation Examples
  *
- *  @desc creating and modifying elements ant attributes  
+ *  @desc creating and modifying elements ant attributes
  *
  * @package Webapplication
  * @module dom
@@ -13,89 +13,122 @@
  */
 
 !(function () {
-    'use strict';
-    // - - - - - - - - - -
-    // DECLARATION / INITIALISATION
+  'use strict';
+  // - - - - - - - - - -
+  // DECLARATION / INITIALISATION
 
-    // FUNCTIONS
+  // FUNCTIONS
 
-    // CONTROL (EVENT CONTROL)
-    console.clear();
-    console.log(window);
-    console.log(window.navigator.userAgent);
-    console.log(window.navigator.appVersion);
-    console.log(window.document);
+  // CONTROL (EVENT CONTROL)
+  console.clear();
+  console.log(window);
+  console.log(window.navigator.userAgent);
+  console.log(window.navigator.appVersion);
+  console.log(window.document);
 
-    // better as object with console.dir!
-    console.dir(document);
-    document.body.bgColor = "white";
-    document.body.style.bgColor = "green";
-    // doesn't work:
-    // window.document.html.body.h1.innerText ="New Headline"
+  // better as object with console.dir!
+  console.dir(document);
+  document.body.bgColor = "white";
+  document.body.style.bgColor = "green";
+  // doesn't work:
+  // window.document.html.body.h1.innerText ="New Headline"
 
-    // use selectors!
-    let h1 = document.querySelector('h1');
-    let arr = document.querySelectorAll('h1');
+  // use selectors!
+  let h1 = document.querySelector('h1');
+  let arr = document.querySelectorAll('h1');
 
-    // using an ID
-    h1 = document.querySelector('#my-headline');
-    h1 = document.getElementById('my-headline');
+  // using an ID
+  h1 = document.querySelector('#my-headline');
+  h1 = document.getElementById('my-headline');
 
-    // modify an object
-    h1.innerText = "A New Headline";
+  // modify an object
+  h1.innerText = "A New Headline";
 
-    // modify an array of objects
-    for (let i = 0; i < arr.length; i += 1) {
-        arr[i].innerText = "A Very New Headline";
+  // modify an array of objects
+  for (let i = 0; i < arr.length; i += 1) {
+    arr[i].innerText = "A Very New Headline";
+  }
+
+  // see object
+  console.log(h1);
+  console.dir(h1);
+
+
+  // other selector commands
+  document.querySelector('h1');
+  document.querySelectorAll('.teaser');
+
+  document.getElementById('my-headline');
+  document.getElementsByName('p');
+
+  let arrByClasses = document.getElementsByClassName('teaser');
+  console.log(arrByClasses.length);
+
+
+  // creating elements
+  let list = document.createElement('ul');
+
+  // creating text nodes
+  let text = document.createTextNode('hello world!');
+
+  // append elements to the DOM
+  document.body.appendChild(list);
+
+  // set attributes
+  list.setAttribute('class', 'navigation');
+
+  let navArray = [
+    ["Home", "index.html"],
+    ["Work", "work.html"],
+    ["Contact", "contact.html"],
+    ["Imprint", "imprint.html"]
+  ];
+
+  for (let i = 0; i < navArray.length; i++) {
+    const element = navArray[i];
+    console.log(element[1]);
+  }
+
+
+  // add a navigation list
+  let nav = document.createElement('nav');
+  let ul = document.createElement('ul');
+
+  let data = [{
+      text: "Home",
+      link: "index.html"
+    },
+    {
+      text: "Work",
+      link: "work.html"
+    },
+    {
+      text: "Contact",
+      link: "contact.html"
+    },
+    {
+      text: "Imprint",
+      link: "imprint.html"
     }
+  ];
 
-    // see object
-    console.log(h1);
-    console.dir(h1);
+  for (let i = 0; i < data.length; i += 1) {
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+    let text1 = document.createTextNode(data[i].text);
+    a.setAttribute('href', data[i].link);
+    a.appendChild(text1);
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
 
-
-    // other selector commands
-    document.querySelector('h1');
-    document.querySelectorAll('.teaser');
-
-    document.getElementById('my-headline');
-    document.getElementsByName('p');
-
-    let arrByClasses = document.getElementsByClassName('teaser');
-    console.log(arrByClasses.length);
-
-
-    // creating elements
-    let list = document.createElement('ul');
-
-    // creating text nodes
-    let text = document.createTextNode('hello world!');
-
-    // append elements to the DOM
-    document.body.appendChild(list);
-
-    // set attributes
-    list.setAttribute('class', 'navigation');
-
-    let navArray = [
-        ["Home", "index.html"],
-        ["Work", "work.html"],
-        ["Contact", "contact.html"],
-        ["Imprint", "imprint.html"]
-    ];
- 
-    for (let i = 0; i < navArray.length; i++) {
-        const element = navArray[i];
-        console.log(element[1]);
-    }
+  nav.appendChild(ul);
+  document.body.appendChild(nav);
 
 
 
 
 
 
-
-
-
-    // - - - - - - - - - -
+  // - - - - - - - - - -
 }());
