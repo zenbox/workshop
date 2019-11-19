aa3e23ab30deb9cfa0ea99037718ff0ba8a2768d
zenbox <michael@zenbox.de>
2019-11-19 15:03:25 +0100

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -17,22 +17,82 @@
   // - - - - - - - - - -
   // DECLARATION
   let componentElementsList = [{
-    element: 'form',
-    context: 'aside'
-  }, {
-    element: 'fieldset',
-    context: 'form'
-  }, {
-    element: 'legend',
-    context: 'fieldset'
-  }, {
-    element: 'label',
-    context: 'fieldset'
-  }];
+      element: 'form',
+      context: 'aside',
+      attributes: {
+        id: 'form-login',
+        method: 'get',
+        action: 'login.php'
+      }
+    }, {
+      element: 'fieldset',
+      context: 'form'
+    }, {
+      element: 'legend',
+      content: 'enter your account',
+      context: 'fieldset'
+    }, {
+      element: 'label',
+      content: 'Email',
+      context: 'fieldset',
+      attributes: {
+        for: 'login-email'
+      }
+    }, {
+      element: 'input',
+      context: 'fieldset',
+      attributes: {
+        type: 'email',
+        id: 'login-email'
+      }
+    },
+    {
+      element: 'label',
+      context: 'fieldset',
+      content: 'Password',
+      attributes: {
+        for: 'login-password'
+      }
+    },
+    {
+      element: 'input',
+      context: 'fieldset',
+      attributes: {
+        type: 'password',
+        id: 'login-password'
+      }
+    },
+    {
+      element: 'input',
+      context: 'fieldset',
+      attributes: {
+        type: 'checkbox',
+        id: 'login-keep-password',
+        class: 'checkbox'
+      }
+    },
+    {
+      element: 'label',
+      content: 'keep password',
+      context: 'fieldset',
+      attributes: {
+        for: 'login-keep-password'
+      }
+    },
+    {
+      context: 'fieldset',
+      element: 'button',
+      content: 'log in',
+      attributes: {
+        type: 'submit'
+      }
+    }
+  ];
 
   // METHODS
   function createElement(element) {
-    let _element = document.createElement(element);
+    let
+      _element = document.createElement(element);
     return _element;
   }
 
@@ -41,24 +101,61 @@
     return _context;
   }
 
-  function appendChild(context) {
-    document.querySelector(context)
-      .appendChild(context);
+  function appendChild(element, context) {
+    let
+      _element = element || undefined,
+      _context = context || undefined;
+
+    try {
+      _context.appendChild(_element);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  function setAttributes(attributes) {}
+  function setAttributes(element, attributes) {
+    for (let key in attributes) {
+      element.setAttribute(key, attributes[key]);
+    }
+  }
 
+  function setContent(element, content) {
+    let
+      _element = element || undefined,
+      _content = document.createTextNode(content) || undefined;
+
+    if (!content || !element) return false;
+
+    element.appendChild(_content);
+    return true;
+  }
+
+  /* TODO use local scope namespaces */
   function createComponent() {
     for (let i = 0; i < componentElementsList.length; i += 1) {
       let
-        element = createElement(componentElementsList[i].element),
-        context = setContext(componentElementsList[i].context);
+        _element, _context,
+        element = componentElementsList[i].element,
+        context = componentElementsList[i].context,
+        attributes = componentElementsList[i].attributes,
+        content = componentElementsList[i].content;
 
-      if (componentElementsList[i].attributes) {
-        setAttributes(element, componentElementsList[i].attributes);
+      _element = createElement(element);
+      _context = setContext(context);
+
+      if (attributes) {
+        setAttributes(_element, attributes);
       }
-      
-      context.appendChild(element);
+
+
+      setContent(_element, content);
+
+      try {
+        appendChild(_element, _context);
+      } catch (error) {
+        console.log(error);
+      }
+
     }
   };
 
