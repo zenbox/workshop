abefd48f72a9925567e59aae992b2478a57414ee
zenbox <michael@zenbox.de>
2019-11-20 14:31:45 +0100

    Update notification.js

--- assets/js/notification.js
+++ assets/js/notification.js
@@ -1,30 +1,55 @@
 !(function () {
-'use strict';
-// - - - - - - - - - -
-// DECLARATION
-// - - - - -
+    'use strict';
+    // - - - - - - - - - -
+    // DECLARATION
+    // - - - - -
+    let
+        notificationElement,
+        config = {
+            defaulttagName: 'div',
+            defaultState: 'default',
+            defaultContext: 'body'
+        };
 
+    // - - - - -
+    // FUNCTIONS
+    // - - - - -
+    function _show(content) {
+        var _content = content || 0;
+        if (typeof (_content) !== 'string') return false;
+        return _content;
+    };
 
-// - - - - -
-// FUNCTIONS
-// - - - - -
-function _double(a) {
-   var _a = a || 0;
+    function _createNotificationElement(tagName) {
+        let _tagName = tagName || config.defaultTagName;
 
-   if (typeof (_a) !== 'number') return NaN;
+        notificationElement = document.createElement(_tagName);
+        return true;
+    }
 
-   return _a + _a;
-};
+    function _appendNotificationElement(context) {
+        let _context = context || config.default.context;
 
-function _main() {
-   // publish module and a function
-   window.module = {} || window.module;
-   window.module.double = _double;
-}
+        _context.appendChild(notificationElement);
+        return true;
+    }
 
-// - - - - -
-// CONTROL
-// - - - - -
-_main();
-// - - - - - - - - - -
+    function _setNotificationState(state) {
+        let _state = state || config.defaultState;
+
+        notificationElement.classlist.addClass(_state);
+        return true;
+    }
+
+    function _main() {
+        // publish module and a function
+        window.notification = {} || window.notification;
+        window.notification.show = _show;
+    }
+
+    // - - - - -
+    // CONTROL
+    // - - - - -
+    _main();
+    // - - - - - - - - - -
 }())
