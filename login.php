0e25334d6a77001821be352fdfcb507559285639
zenbox <michael@zenbox.de>
2019-11-20 20:27:53 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,13 +1,11 @@
 <?php
-
-$string .= "{";
-
-foreach($_GET AS $key => $value) {
-    $string .= "\"$key\" : \"$value\", ";
+$string = '';
+if (count($_GET) >0) {
+  $string .= "{";
+  foreach($_GET AS $key => $value) {
+      $string .= "\"$key\" : \"$value\", ";
+  }
+  $string = substr($string, 0, strlen($string) - 2);
+  $string .= "}";
+  echo $string;
 }
-
-$string = substr($string, 0, strlen($string) - 2);
-
-$string .= "}";
-
-echo $string;
