21e85b362735b2ea004ea02ab9cf1781131c4c1f
zenbox <michael@zenbox.de>
2019-12-09 13:36:22 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+/** lorem ...
+  *
+  *  @desc lorem
+  *
+  * @package Webapplication
+  * @module first_app
+  * @author Michael <michael.reichart@gfu.net>
+  * @version v1.0.0
+  * @since 2019-12-09
+  * @see i.e. inspired by ... {link to}
+  * @license MIT {https://opensource.org/licenses/MIT}
+  * @copyright (c) 2019 Michael Reichart, Cologne
+  */
