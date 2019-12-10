2c7440f7a657b187f6afdddc8673b78b44a05402
zenbox <michael@zenbox.de>
2019-12-10 08:50:56 +0100

    Create server.js

---
+++ express-server/server.js
@@ -0,0 +1,19 @@
+/** An simple Express Webservice
+ *
+ * @package Webapplication
+ * @module Webservice
+ * @author Michael <michael.reichart@gfu.net>
+ * @version v1.0.0
+ * @since 2019-12-09
+ * @see i.e. inspired by ... {link to}
+ * @license MIT {https://opensource.org/licenses/MIT}
+ * @copyright (c) 2019 Michael Reichart, Cologne
+ */
+
+let
+    // node modules
+    http = require('http'),
+    path = require('path'),
+
+    // npm modules
+    express = require('express');
