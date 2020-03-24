7afccea7d3a901834edad63acf63e38157a8574a
zenbox <michael@zenbox.de>
2020-03-24 11:27:51 +0100

    Update scopes.js

--- static/scopes.js
+++ static/scopes.js
@@ -1,3 +1,6 @@
+
+
+
 /* eslint-disable no-inner-declarations */
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
@@ -14,13 +17,50 @@ let themes = {};
 
 
 // - - - - - - - - - -
-setTheme(title = 'Begrüßung');
+setTheme(title = 'scopes');
 // - - - - - - - - - -
 if (eval(themes[title])) {
     // - - - - - - - - - -
-    
-    let a = "Guten Morgen";
+
+    var old = true;
+
+    // window.i !!
+    for (var i = 0; i < 10; i++) { 
+        // console.log(i);
+    }
+
+    // Control block scoped!
+    let a = 42;
     log(a);
+
+    // - - - - - - - - - -
+}
+
+// - - - - - - - - - -
+setTheme(title = 'constants');
+// - - - - - - - - - -
+if (eval(themes[title])) {
+    // - - - - - - - - - -
+
+    // for number, string, boolean
+    const PI = 3.1415;
+    const TRUE = true;
+    const string = "any fixed text";
+
+    // ! error
+    // PI = 2;
+
+    // for objects and array
+    const obj = {key : "value"};
+
+    obj['new_key'] = "new value";
+    console.log(obj);
+
+    // node code example
+    // const http = require('http');
+
+    // ! error
+    // obj = [];
 
     // - - - - - - - - - -
 }
