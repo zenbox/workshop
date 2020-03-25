b134d6eba1e2ae1ed6f9add54ea3f6d146afcab4
zenbox <michael@zenbox.de>
2020-03-25 12:54:46 +0100

    Update modules.js

--- static/modules.js
+++ static/modules.js
@@ -1,69 +1,70 @@
-// - - - - - - - - - -
-setTheme(title = 'modules');
-// - - - - - - - - - -
-if (eval(themes[title])) {
-    // - - - - - - - - - -
-
-    // old way
-    // IIFE Pattern
-    !(function () {
-        'use strict'
-        // - - - - - - - - - -
-        var a = 42;
-        log('works!');
-        // - - - - - - - - - -
-    }());
-
-    ! function () {
-        log('works too!');
-    }();
-
-    +
-    function () {
-        log('works too!');
-    }();
-
-    // module block pattern?
-    !(function () {
-        'use strict'
-        // - - - - - - - - - -
-        var _a = 42;
-        log('works!');
-
-        window.myModule = window.myModule || {};
-        window.myModule.a = _a;
-
-        // - - - - - - - - - -
-    }());
-
-    !(function (module) {
-        'use strict'
-        // - - - - - - - - - -
-        var b = module.a;
-        log('works!');
-
-        window.myModule = window.myModule || {};
-        // - - - - - - - - - -
-    }(window.myModule));
+    // new way for modules
+    // capsule, scope, collaborative
+    import User, {
+        Male,
+        Female
+    } from './components/user.js';
 
     // - - - - - - - - - -
-}
+    setTheme(title = 'module');
 
-// $(document).ready(function(){
-// 'use strict';
-//  - - - - - - - - - - -
-//  - - - - - - - - - - -
-// });
+    // - - - - - - - - - -
+    if (eval(themes[title])) {
+        // - - - - - - - - - -
+        // old way
+        // IIFE Pattern
+        !(function () {
+            'use strict'
+            // - - - - - - - - - -
+            var a = 42;
+            log('works!');
+            // - - - - - - - - - -
+        }());
 
-// new way for modules
-// capsule, scope, collaborative
-import User, {
-    Male,
-    Female
-} from './components/user.js';
+        ! function () {
+            log('works too!');
+        }();
 
-let john = new User('Doe', 'John', 'john@doe.org');
-log(john.prename);
+        +
+        function () {
+            log('works too!');
+        }();
 
-let ann = new Female('Doe', 'Ann', 'ann@doe.org');
-log(ann.prename)
+        // module block pattern
+        !(function () {
+            'use strict'
+            // - - - - - - - - - -
+            var _a = 42;
+            log('works!');
+
+            window.myModule = window.myModule || {};
+            window.myModule.a = _a;
+
+            // - - - - - - - - - -
+        }());
+
+        !(function (module) {
+            'use strict'
+            // - - - - - - - - - -
+            var b = module.a;
+            log('works!');
+
+            window.myModule = window.myModule || {};
+            // - - - - - - - - - -
+        }(window.myModule));
+
+        // new way for modules
+        // capsule, scope, collaborative
+        // @see line 1:
+        // import User, {
+        //     Male,
+        //     Female
+        // } from './components/user.js';
+
+        let john = new User('Doe', 'John', 'john@doe.org');
+        log(john.prename);
+
+        let ann = new Female('Doe', 'Ann', 'ann@doe.org');
+        log(ann.prename)
+        // - - - - - - - - - -
+    }
