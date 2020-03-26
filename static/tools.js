b857a0d30044c58bdc5f5f256f06d78a7158d99f
zenbox <michael@zenbox.de>
2020-03-26 07:24:49 +0100

    Update tools.js

--- static/tools.js
+++ static/tools.js
@@ -5,12 +5,12 @@
     'use strict';
     var assert = (assertion = undefined, description = undefined) => {
         var
-        line = '',
-        prefix = 'assert for';
+            line = '',
+            prefix = 'assert for';
         if (typeof assertion === 'undefined') return false;
         if (title) {
             line = '-'.repeat(title.length + prefix.length);
-            
+
             console.log('');
             console.log(prefix, title);
             console.log(description);
@@ -18,7 +18,7 @@
         console.log(assertion);
         console.log(line);
     };
-    
+
     window.assert = assert;
 }
 
@@ -27,8 +27,7 @@
     let count = 1;
 
     var log = (...message) => {
-        let line = '',
-            style =
+        let style =
             'color: grey;' +
             'font-size: 8px;' +
 
@@ -45,23 +44,14 @@
             '';
 
         // console.clear();
-        // if (typeof message === 'undefined') return false;
+
         if (title) {
-            line = '-'.repeat(title.length);
-
             console.log('');
-            console.log(`%c${title.toUpperCase()} ${count++}`, style);
-            // console.log(`%c${line}`, style);
+            console.log(`%c(${count++}) ${title.toUpperCase()}`, style);
         }
-        // console.log('is array:', Array.isArray(message));
-        // console.log('type:', typeof message);
 
-        // if (Array.isArray(message) === true) {
-        //     console.log('array');
-        // for (let i = 0; i < message.length; i += 1) {
-        //     console.log(message[i]);
-        // }
         let i = 1;
+
         if (Array.isArray(message[i])) {
             let m = 'arr -> ';
 
@@ -69,18 +59,12 @@
                 m += message[i][j] + ' ';
             }
             console.log(m)
-        } else { 
-        
-        for (let key in message) {
+        } else {
+
+            for (let key in message) {
                 console.log(`${message[key]}`);
+            }
         }
-}
-        // } else if (typeof message === 'object') {
-        // } else {
-        //     console.log(typeof message);
-        //     console.log(message);
-        // }
-        // console.log(`%c${line}`, style);
     };
 
     window.log = log;
