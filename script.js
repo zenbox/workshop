c48275569f9cb84af52f1f08f606ef71095fb14d
zenbox <michael@zenbox.de>
2020-04-21 16:26:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,5 +18,41 @@
         document
             .querySelector('html')
             .classList.toggle('bigger-font');
-    })
+    });
+
+    // Own messages
+    // setCustomValidity(message)
+
+    const customMessages = {
+        valueMissing: 'Gib was ein!', // `required` attr
+        emailMismatch: 'Das ist mal keine Email Adresse!', // Invalid email
+        patternMismatch: 'Versuch was anderes', // `pattern` attr
+    }
+
+    function getCustomMessage(type, validity) {
+        if (validity.typeMismatch) {
+            return customMessages[`${type}Mismatch`]
+        } else {
+            for (const invalidKey in customMessages) {
+                if (validity[invalidKey]) {
+                    return customMessages[invalidKey]
+                }
+            }
+        }
+    }
+
+    var inputs = document.querySelectorAll('input, select, textarea')
+    inputs.forEach(function (input) {
+        // Each time the user types or submits, this will
+        // check validity, and set a custom message if invalid.
+        function checkValidity() {
+            const message = input.validity.valid ?
+                null :
+                getCustomMessage(input.type, input.validity, customMessages)
+            input.setCustomValidity(message || '')
+        }
+        input.addEventListener('input', checkValidity)
+        input.addEventListener('invalid', checkValidity)
+    });
+
 }
