abcc0037df50e0ea5b2b98a9c86aab39fca525c2
zenbox <michael@zenbox.de>
2020-06-17 12:35:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,37 @@ setTheme(title = 'scopes');
 // - - - - - - - - - -
 if (eval(themes[title])) {
     // - - - - - - - - - -
-    var a = 42;
+    var a = 42.3, i;
     let b = 108;
+
+    const PI = 3.1415;
+  
+    const obj = {};
+    const arr = [];
+    const fn = function () {};
+
+
+    log(typeof fn);
+    log(typeof obj);
+    log(typeof arr);
+    log(typeof a);
+
+
+    log(21 - '1');
+
+  
+
+    obj.member = 512;
+console.log(obj)
+
 
     log('window.a:', window.a);
     log('window.b:', window.b);
     log('outer > a,b', a, b);
+
+    // for (let i = 0; i < 10; i += 1) {
+    //     log(i);
+    // }
 
     function ownFunction() {
         if (true) {
@@ -19,5 +44,34 @@ if (eval(themes[title])) {
     }
 
     ownFunction();
+    // - - - - - - - - - -
+}
+
+
+
+
+// - - - - - - - - - -
+setTheme(title = 'functions');
+// - - - - - - - - - -
+if (eval(themes[title])) {
+    // - - - - - - - - - -
+    
+
+    function fnName() { 
+        log('fnName');
+    }
+    
+    let fn = function () { 
+        log('fn');
+    };
+
+    let arrFn = () => { log('arrow function'); }
+
+    fnName();
+    fn();
+    arrFn();
+
+
+    log(this)
     // - - - - - - - - - -
 }
