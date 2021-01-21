13f7456c22b3898a7106dde1318eb415596a578a
zenbox <michael@zenbox.de>
2021-01-21 11:39:04 +0100

    Create server.js

---
+++ chat/server.js
@@ -0,0 +1,13 @@
+/** 
+  *
+  *  @desc 
+  *
+  * @package Webapplication
+  * @module 
+  * @author Michael <michael.reichart@gfu.net>
+  * @version v1.0.0
+  * @since 2021-01-21
+  * @see i.e. inspired by ... {link to}
+  * @license MIT {https://opensource.org/licenses/MIT}
+  * @copyright (c) 2021 Michael Reichart, Cologne
+  */
