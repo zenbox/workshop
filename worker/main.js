3408f3d022f53d72bef5625d645bdc582afcc2c6
zenbox <michael@zenbox.de>
2021-01-21 09:54:02 +0100

    Update main.js

--- worker/main.js
+++ worker/main.js
@@ -1,20 +1,49 @@
 (function () {
 
-    let worker = new Worker('task.js');
+    let worker = new Worker('task.js'),
+        statusDisplay,
+        searchButton;
 
-    worker.onerror = () => {console.log('something went wrong.')};
+    window.onload = function () {
+        statusDisplay = document.querySelector('#statusDisplay');
+        searchButton = document.querySelector('#searchButton');
+    }
+
+    doSearch = function () {
+        let fromNumber = document.querySelector('#from').value,
+            toNumber = document.querySelector('#to').value;
+
+
+        worker.postMessage({
+            "from": fromNumber,
+            "to": toNumber
+        });
+
+        statusDisplay.innerHTML = `The worker is on the job (${fromNumber} , ${toNumber})`;
+
+    }
+
+    cancelSearch = function () {
+        worker.terminate();
+        worker = null;
+        statusDisplay.innerHTML = `Search cancelled`;
+    }
+
+
+    worker.onerror = () => {
+        console.log('something went wrong.')
+    };
 
     worker.onmessage = function (event) {
         console.log('worker started ...');
         console.dir(event.data.message);
     };
 
-    worker.onclose = () => { console.log('worker closed!')};
+    worker.onclose = () => {
+        console.log('worker closed!')
+    };
 
-        worker.postMessage({
-            "message": "hello worker"
-        });
 
-        worker.terminate();
+
 
 }())
