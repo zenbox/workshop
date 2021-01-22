8cc21352362d09562ed34f66920459eefced81a0
zenbox <michael@zenbox.de>
2021-01-22 09:11:34 +0100

    Update users.js

---
+++ chat/utils/users.js
@@ -0,0 +1,16 @@
+const users = [];
+
+function userJoin() {}
+
+function userLeave() {}
+
+function getCurrentUser() {}
+
+function getRoomUsers() {}
+
+module.exports = {
+    userJoin,
+    userLeave,
+    getCurrentUser,
+    getRoomUsers,
+}
