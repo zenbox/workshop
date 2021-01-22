80688b5ded888140d01cf7553162b44d716f544d
zenbox <michael@zenbox.de>
2021-01-22 09:50:45 +0100

    Update users.js

--- chat/utils/users.js
+++ chat/utils/users.js
@@ -1,16 +1,32 @@
 const users = [];
 
-function userJoin() {}
+function userJoin(id, username, room) {
+    const user = {
+        id,
+        username,
+        room
+    }
+    users.push(user);
 
-function userLeave() {}
+    return user;
+}
 
-function getCurrentUser() {}
+function userLeave(id) {
+    const index = users.findIndex(user => user.id === id);
+    if (index !== -1) return users.splice(index, 1)[0];
+}
 
-function getRoomUsers() {}
+function getCurrentUser(id) {
+    return users.findIndex(user => user.id === id);
+}
+
+function getRoomUsers(room) {
+    return users.filter(user => user.room === room);
+}
 
 module.exports = {
     userJoin,
     userLeave,
     getCurrentUser,
-    getRoomUsers,
+    getRoomUsers
 }
