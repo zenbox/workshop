963bf6eb8bb6ba8c60d6f3e36b10e1aab98b9d82
zenbox <michael@zenbox.de>
2021-03-09 13:52:34 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
-setInterval(() => {
-    document
-        .querySelector('.content')
-        .classList
-        .toggle('content--aside-open')
-}, 3000);
+/** A SIMPLE UI CONTROL
+ *
+ * @package Webapplication
+ * @module 
+ * @author Michael <michael.reichart@gfu.net>
+ * @version v1.0.0
+ * @since 2021-03-09
+ * @see i.e. inspired by ... {link to}
+ * @license MIT {https://opensource.org/licenses/MIT}
+ * @copyright (c) 2021 Michael Reichart, Cologne
+ */
+
+
