90da4d22b9fa1e3a19b91051b43e2dbafaa11b2d
zenbox <michael@zenbox.de>
2021-06-14 15:35:08 +0200

    Update index.js

---
+++ express-server/index.js
@@ -0,0 +1,37 @@
+/** Webservice Application Index Script
+ *
+ * @desc Lorem ipsum dolor
+ *       sit amet consectetur ...
+ *
+ * @package   Webservice Application
+ * @module    init
+ * @author    Michael <michael.reichart@gfu.net>
+ * @version   v0.1.0
+ * @since     2021-06-14
+ * @see       i.e. inspired by ... {link to}
+ * @license   MIT {https://opensource.org/licenses/MIT}
+ * @copyright (c) 2021 Michael Reichart, Cologne
+ */
+
+// Declaration
+// node installed modules
+const http = require('http');
+const path = require('path');
+
+// npm modules
+// ./node_modules/express/index.js
+const express = require('express');
+const expressServer = express();
+const bodyParser = require('body-parser');
+
+// Variables (configuration)
+let
+  port = 3000,
+  indexRoute = require(path.join(__dirname, '/routes/index.js')),
+  searchRoute = require(path.join(__dirname, '/routes/search.js'));
+
+
+
+
+// Control
+expressServer.listen(port, () => console.log(`Express server runs on port ${port}.`));
