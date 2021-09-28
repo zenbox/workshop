92f4613886b88ff2246c0f4fd45240635df0e930
zenbox <michael@zenbox.de>
2021-09-28 12:08:46 +0200

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let templateContent = document.querySelector('.template-content');
+let hamburgerButton = document.querySelector('#hamburger-button');
+
+hamburgerButton.addEventListener('click', (event) => {});
+templateContent.classList.toggle('active');
