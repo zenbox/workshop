690aaa78bea6367ac32dcd62319d5a2781443c03
zenbox <michael@zenbox.de>
2021-10-25 08:38:27 +0200

    init

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-let templateContent = document.querySelector('.template-content');
-let hamburgerButton = document.querySelector('#hamburger-button');
-
-hamburgerButton.addEventListener('click', (event) => {
-    templateContent.classList.toggle('active');
-});
--- shoplayout/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-let likeElements = document.querySelectorAll('.like');
-
-for (let i = 0; i < likeElements.length; i += 1) {
-    likeElements[i].addEventListener('click', (event) => {
-        event.target.classList.toggle('like-default');
-        event.target.classList.toggle('like-selected');
-    })
-}
