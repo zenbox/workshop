884ded5f402a5d21f365bb086188d872467edb53
zenbox <michael@zenbox.de>
2022-02-08 14:21:49 +0100

    Explain the IIFE pattern

---
+++ assets/js/footer.js
@@ -0,0 +1,10 @@
+// IIFE - Immediate Invoked Function Expression
+// build a scope!
+(function () {
+    // - - - - - - - - - -
+
+    var a = 43;
+    console.log('footer:', a);
+
+    // - - - - - - - - - -
+}())
