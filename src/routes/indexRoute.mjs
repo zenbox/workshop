c872d4caaae73c02e71147e8135651dacc5ee8f8
zenbox <michael@zenbox.de>
2022-10-10 16:06:41 +0200

    Update indexRoute.mjs

--- src/routes/indexRoute.mjs
+++ src/routes/indexRoute.mjs
@@ -19,7 +19,8 @@ import IndexController from "./../controller/IndexController.class.mjs";
 const router = express.Router();
 
 // Routes under / (root)
-let indexRoutes = ["/", "/page"];
+let indexRoutes = ["/", "/page"],
+  indexController = new IndexController();
 
 indexRoutes.forEach((route) => {
   router.get(route, (request, response, next) => {
