0b1d661cbb38256ceb1293f1bb2d7a81bb489e0d
zenbox <michael@zenbox.de>
2022-10-10 14:53:37 +0200

    Create indexRoute.mjs

---
+++ src/routes/indexRoute.mjs
@@ -0,0 +1,30 @@
+/** Index Routes
+ *
+ * @package Webapplication
+ * @module
+ * @author Michael <michael.reichart@gfu.net>
+ * @version v1.0.0
+ * @since 2022-10-10
+ * @see i.e. inspired by ... {link to}
+ * @license MIT {https://opensource.org/licenses/MIT}
+ * @copyright (c) 2022 Michael Reichart, Cologne
+ */
+
+import express from "express";
+// - - - - - - - - - -
+import IndexController from "./../controller/IndexController.class.mjs";
+// - - - - - - - - - -
+
+// Route config
+const router = express.Router();
+
+// Routes under / (root)
+let indexRoutes = ["/", "/page"];
+
+indexRoutes.forEach((route) => {
+  router.get(route, (request, response, next) => {
+    indexController.getPage(request, response, next);
+  });
+});
+
+export default router;
