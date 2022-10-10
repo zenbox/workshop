64f9d69f7297d58da0c99182908fff195795bc30
zenbox <michael@zenbox.de>
2022-10-10 16:06:37 +0200

    Update IndexController.class.mjs

--- src/controller/IndexController.class.mjs
+++ src/controller/IndexController.class.mjs
@@ -1,7 +1,12 @@
 export default class IndexController {
   constructor() {}
 
-  getPage() {
+  getPage(request, response, next) {
+    let view = "index",
+      data = ["one", "two", "three"];
+
+    response.status(200).render(view, { data: data });
+
     return true;
   }
 }
