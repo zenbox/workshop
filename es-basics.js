dc42d22899510b6dd291476ae09bbf3e7bb61051
zenbox <michael@zenbox.de>
2022-10-11 12:24:53 +0200

    Update es-basics.js

--- es-basics.js
+++ es-basics.js
@@ -50,3 +50,42 @@ MyClass.count();
 const myInstance = new MyClass(42);
 
 console.log(myInstance.value); // 42
+
+// Let vs. var
+("use strict");
+
+// var -> function scope
+// let -> control scope
+var c = undefined;
+
+function doThis() {
+  // hoisting (to hoist : nach oben ziehen)
+  let a = 42;
+
+  for (let i = 0; i < 8; i++) {
+    console.log(i); // 0, 1, 2, ...
+  }
+
+  // function scope, 'cause ...
+  var b = 108;
+
+  var c = 512;
+
+  console.log(i); // 8
+  console.log(a);
+}
+
+doThis(); // 42
+
+const PI = 3.1415982; // Primitive value!
+
+PI = 0; // error!
+
+const obj = {
+  key: "value",
+};
+
+obj.newKey = "newValue"; // no error?
+obj["newKey"] = "newValue";
+
+obj = 42; // error!
