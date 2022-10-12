a8d83f73bb6f0db881225c7bd1d393992655b7da
zenbox <michael@zenbox.de>
2022-10-12 08:44:16 +0200

    Update sql.mjs

--- sql.mjs
+++ sql.mjs
@@ -1,43 +1,11 @@
-import mysql from "mysql"
-
-const dbConfig = {
-    host: "127.0.0.1",
-    port: "3306",
-    user: "root",
-    password: "miromiro",
-    database: "animals",
-}
-
-let db = mysql.createConnection(dbConfig),
-    sql = null
-
-db.on("error", function (error) {
-    console.log(error)
-})
-
-db.query("CREATE DATABASE IF NOT EXISTS animals;")
-db.query("USE animals;")
-
-// Tabelle anlegen
-db.query("DROP TABLE IF EXISTS sheeps;")
-
-sql =
-    "CREATE TABLE sheeps ( " +
-    "id INT(11) AUTO_INCREMENT, " +
-    "sheep VARCHAR(50), " +
-    "PRIMARY KEY (id) );"
-db.query(sql)
-
-sql =
-    "INSERT INTO sheeps " +
-    "(sheep) " +
-    "VALUES " +
-    "('Merinoschaf')," +
-    "('Bergschaf')," +
-    "('Haarschaf');"
-
-db.query(sql, function () {
-    console.log("3 Datensätze geschrieben.")
-})
-
-db.end()
+/** Database Initialisation
+ *
+ * @package Webapplication
+ * @module Database
+ * @author Michael <michael.reichart@gfu.net>
+ * @version v1.0.0
+ * @since 2022-10-12
+ * @see i.e. inspired by ... {link to}
+ * @license MIT {https://opensource.org/licenses/MIT}
+ * @copyright (c) 2022 Michael Reichart, Cologne
+ */
