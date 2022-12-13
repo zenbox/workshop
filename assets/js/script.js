51a2a35f3a156ad58c0a062307b17d88822bc50f
zenbox <michael@zenbox.de>
2022-12-13 15:05:38 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,24 @@
-const data = {
-  UUID: "{02397efwkhsf97423hefs}",
-  KanalName: "PMX_107 Ch4.4",
-  Kurzbezeichung: "",
-  "Physikalische Einheit": " ",
-  Formatierung: "7:F3",
-  Kommentar: "",
-  ArtDesKanals: "",
-  Werte: [
-    "2022-12-12T12:00:00.1234Z",
-    [0.2342345, 0.76435123, 0.98128723, 0.12387345, 0.534813286],
-  ],
-};
+const data = [
+  {
+    UUID: "{02397efwkhsf97423hefs}",
+    KanalName: "PMX_107 Ch4.4",
+    Kurzbezeichung: "",
+    "Physikalische Einheit": " ",
+    Formatierung: "7:F3",
+    Kommentar: "",
+    ArtDesKanals: "",
+  },
+  {
+    UUID: "{023efa45z97efwkhss}",
+    KanalName: "PMX_142 Ch5.4",
+    Kurzbezeichung: "",
+    "Physikalische Einheit": " ",
+    Formatierung: "7:F3",
+    Kommentar: "",
+    ArtDesKanals: "",
+  },
+  ["2022-12-12T12:00:00.1234Z", [0.2342345, 0.76435123]],
+];
 
 console.log(document);
 console.dir(document);
