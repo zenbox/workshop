3c1e02075c69a51bb72820865297a7d2930fa1d7
zenbox <michael@zenbox.de>
2022-12-13 14:56:27 +0100

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const data = {
+  UUID: "{02397efwkhsf97423hefs}",
+  KanalName: "PMX_107 Ch4.4",
+  Kurzbezeichung: "",
+  "Physikalische Einheit": " ",
+  Formatierung: "7:F3",
+  Kommentar: "",
+  ArtDesKanals: "",
+  Werte: [
+    "2022-12-12T12:00:00.1234Z",
+    [0.2342345, 0.76435123, 0.98128723, 0.12387345, 0.534813286],
+  ],
+};
+
+console.log(document);
+console.dir(document);
+
+let h1 = document.querySelector("h1");
+h1.innerHTML = "<em>Test</em>";
