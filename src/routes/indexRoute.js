077b44e7c37b09abfab5008fb1a795439f0c1777
zenbox <michael@zenbox.de>
2022-12-20 16:10:38 +0100

    Update indexRoute.js

--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -1,17 +1,22 @@
 import express from "express";
 // - - - - - - - - - -
 import IndexController from "../controller/IndexController.class.js";
+import SheepsController from "../controller/SheepsController.class.js";
 // - - - - - - - - - -
 const router = express.Router();
 // - - - - - - - - - -
-
 const indexController = new IndexController();
-
+const sheepsController = new SheepsController();
+// - - - - - - - - - -
 router.get("/", (request, response, next) => {
     indexController.getPage(request, response, next);
 });
 
-router.post("page-2/state/")
+router.get("/sheeps", (request, response, next) => {
+    sheepsController.selectAllSheeps(request, response, next);
+});
+
+// router.post("page-2/state/");
 
 // - - - - - - - - - -
 export default router;
