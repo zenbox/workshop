11601ab524d6018d5d6dcc2b631dbc0574e5c111
zenbox <michael@zenbox.de>
2022-12-20 10:33:41 +0100

    Update indexRoute.js

--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 // - - - - - - - - - -
-import IndexController from "./../controller/indexController.class.js";
+import IndexController from "../controller/IndexController.class.js";
 // - - - - - - - - - -
 const router = express.Router();
 // - - - - - - - - - -
@@ -10,6 +10,8 @@ const indexController = new IndexController();
 router.get("/", (request, response, next) => {
     indexController.getPage(request, response, next);
 });
+
+router.post("page-2/state/")
 
 // - - - - - - - - - -
 export default router;
