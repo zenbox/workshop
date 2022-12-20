1d8cca021fc5d85a6f1defa23b7dc1893b473a9f
zenbox <michael@zenbox.de>
2022-12-20 16:10:44 +0100

    Create dbConfig.js

---
+++ src/model/dbConfig.js
@@ -0,0 +1,11 @@
+const dbConfig = {
+    host: "localhost",
+    user: "root",
+    password: "miromiro",
+    port: 3306,
+    database: "animals",
+
+    insecureAuth: true,
+};
+
+export default dbConfig;
