1f758a445e1988d9fac580981fc96012a38d8d4c
zenbox <michael@zenbox.de>
2023-01-09 08:59:49 +0100

    init

--- hello-world.js
+++
@@ -1,5 +0,0 @@
-function log(message) {
-    console.log(message);
-}
-
-log("hello world");
--- src/controller/indexController.class.js
+++
@@ -1,8 +0,0 @@
-export default class IndexController {
-    constructor() {}
-    getPage(request, response, next) {
-        let view = "indexView",
-            data = { list: ["one", "two", "three"] };
-        response.status(200).render(view, data);
-    }
-}
