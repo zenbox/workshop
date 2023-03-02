bec24743a7f412b8c1c3ec99dd8ce2db53411c46
zenbox <michael@zenbox.de>
2023-03-02 09:19:21 +0100

    Update with ES modules

--- wwwroot/assets/js/we-data.js
+++ wwwroot/assets/js/we-data.js
@@ -10,83 +10,8 @@
  * @copyright (c) 2023 Michael Reichart, Cologne
  */
 
-class DataGenerator {
-    constructor() {
-        return true;
-    }
-
-    /**
-     * @desc    Generates a random value between 0 and 9999
-     * @returns {number}
-     */
-    fnCreateDatapointValue() {
-        let nValue = undefined;
-        nValue = Math.round(Math.random() * 9999);
-
-        return nValue;
-    }
-
-    fnUpdateDatapointValue() {}
-
-    /**
-     * @desc    Set any oDatapoint into a grid table
-     * @param   {object} oDatapoint - contains grid infos and an optional value
-     * @returns {void}
-     */
-    fnSetDatapoint(oDatapoint) {
-        let nSection = oDatapoint.section,
-            nRow = oDatapoint.row,
-            nValue = oDatapoint.value,
-            sDescription = oDatapoint.description;
-
-        if (nValue === undefined) {
-            nValue = this.fnCreateDatapointValue();
-        }
-
-        document.querySelector(
-            `#we-${nSection} [data-value="${nRow}"]`
-        ).textContent = nValue;
-
-        document.querySelector(
-            `#we-${nSection} [data-unit="${nRow}"]`
-        ).textContent = sDescription;
-
-        // $(`#we-${nSection} [data-value=${nRow}]`).text(nValue);
-        // $(`#we-${nSection} [data-unit=${nRow}]`).text(sDescription);
-    }
-}
-
-class TableBuilder {
-    constructor(sDataContext) {
-        this.sDataContext = sDataContext;
-    }
-
-    fnGetTemplateTable() {
-        this.templateTable =
-            document.querySelector("template").content.children[0];
-    }
-
-    fnSetTemplateTable() {
-        document
-            .querySelector(this.sDataContext)
-            .appendChild(this.templateTable);
-    }
-
-    fnSetDatapoint(nDataIndex, oDatapoint) {
-        let nValue = oDatapoint.value,
-            sUnit = oDatapoint.unit;
-
-        let oValueField = this.templateTable.querySelector(
-                `[data-value="${nDataIndex}"]`
-            ),
-            oUnitField = this.templateTable.querySelector(
-                `[data-unit="${nDataIndex}"]`
-            );
-
-        oValueField.textContent = nValue;
-        oUnitField.textContent = sUnit;
-    }
-}
+import DataGenerator from "./classes/DataGenerator.class.js";
+import TemplateTable from "./classes/TemplateTable.class.js"
 
 const dataset = [];
 
@@ -119,9 +44,9 @@ $(document).ready(() => {
 
     // Data ...
     let nDataIndex = 1,
-        sDataContext = "#we-4";
+        sDataContext = "#we-3";
 
-    let oTableBuilder = new TableBuilder(sDataContext);
+    let oTableBuilder = new TemplateTable(sDataContext);
 
     oTableBuilder.fnGetTemplateTable();
 
