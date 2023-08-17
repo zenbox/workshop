cc97460e32da71a31f88a3b14fa44582eab695da
zenbox <michael@zenbox.de>
2023-08-17 09:30:53 +0200

    add photodiode sensor

--- led-button-potentiometer/led-button-potentiometer.ino
+++ led-button-potentiometer/led-button-potentiometer.ino
@@ -8,14 +8,17 @@
 // integer sind 2 Byte lang, 0 - 65535
 
 const boolean DEBUG = false;
-
-const byte LED_PIN = 3; // 0 - 255
+// int braucht 2 bytes, eines reicht aber völlig aus!
+const byte LED_PIN = 3; // 0 - 255, 1 byte (8 bit)
 const byte POT_PIN = A1;
 const byte BTN_PIN = 5;
+const byte LS_PIN = A0;
 
 boolean ledState;
 boolean btnState;
+// unsigned: OHNE Vorzeichen, also keine Minus-Werte!
 unsigned int potValue;
+unsigned int lsValue;
 
 void setup() {
   // Serielle Ausgabe am Rechner
@@ -25,21 +28,29 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);
   pinMode(BTN_PIN, INPUT);
   // überflüssig: pinMode(A1, INPUT)
+  // überflüssig: pinMode(A0, INPUT)
 
   // Aktuelle Zustände der Bauteile
   ledState = LOW;
   digitalWrite(LED_PIN, ledState); // Zustand auf aus setzen
   btnState = digitalRead(BTN_PIN); // Zustand abfragen
   potValue = analogRead(POT_PIN);
-
+  lsValue = analogRead(LS_PIN);
+  
   // Serielle Ausgabe eines Wertes
   if (DEBUG) Serial.println(potValue);
 }
 
 void loop() {
-  // Wiederholte serielle Ausgabe eines Wertes
+  // Wiederholtes Einlesen eines Wertes
   potValue = analogRead(POT_PIN);
+  lsValue = analogRead(LS_PIN);
   btnState = digitalRead(BTN_PIN);
+
+// Testausgabe des LS Wertes
+Serial.print("LS_PIN: ");
+Serial.print(lsValue);
+Serial.println();
 
   if (btnState == HIGH) {
     Serial.println("Der Taster ist gedrückt.");
@@ -50,17 +61,17 @@ void loop() {
   }
 
   // BEREICH ABFRAGEN
-//  if (potValue >= 0 && potValue <= 100) {
-//    Serial.println("Das Potentiometer ist zwischen 0 und 100.");
-//  } else if (potValue > 100 && potValue <= 400) {
-//    Serial.println("Das Potentiometer ist zwischen 101 und 400.");
-//  }
+  //  if (potValue >= 0 && potValue <= 100) {
+  //    Serial.println("Das Potentiometer ist zwischen 0 und 100.");
+  //  } else if (potValue > 100 && potValue <= 400) {
+  //    Serial.println("Das Potentiometer ist zwischen 101 und 400.");
+  //  }
 
   if (btnState == HIGH) {
-    
-    int dimmedPotValue = map(potValue, 0,1023, 0,255); // 0 (LOW) - 255 (HIGH)
-    analogWrite(LED_PIN, dimmedPotValue); 
-    
+
+    int dimmedPotValue = map(potValue, 0, 1023, 0, 255); // 0 (LOW) - 255 (HIGH)
+    analogWrite(LED_PIN, dimmedPotValue);
+
   } else {
     digitalWrite(LED_PIN, LOW);
   }
@@ -69,6 +80,15 @@ void loop() {
   if (DEBUG) Serial.print(" / ");
   if (DEBUG) Serial.print( potValue );
   if (DEBUG) Serial.println();
+
+  // - - - - -
+  float fDeg = 21.3;
+  String sUnit = "Celsius";
+
+  char buffer[40];
+  sprintf(buffer, "Temp: %d degree %s", fDeg, sUnit);
+  Serial.println(buffer);
+  // - - - - -
 
   delay(500);
 }
