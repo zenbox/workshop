1215ae823bcee7ece62e79868f5520408df5544c
zenbox <michael@zenbox.de>
2023-10-25 10:53:57 +0200

    update with new gitignore

--- nextjs-tutorial/pages/_app.js
+++
@@ -1,5 +0,0 @@
-import "../styles/style.global.scss";
-
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-}
--- react-router-tutorial/src/routes/destroy.jsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "react-router-dom";
-import { deleteContact } from "../contacts";
-
-export async function action({ params }) {
-  // throw new Error("oh dang!");
-
-  await deleteContact(params.contactId);
-  return redirect("/");
-}
