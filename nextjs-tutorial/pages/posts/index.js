3cd266e625700a901b1e93e3752b6e5470a1435a
zenbox <michael@zenbox.de>
2023-11-13 08:52:34 +0100

    Init

--- nextjs-tutorial/pages/posts/index.js
+++
@@ -1,3 +0,0 @@
-export function Index() {
-    return <div>Index</div>;
-}
--- src/lib/getData.tsx
+++
@@ -1,4 +0,0 @@
-export async function getData() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    return response.json();
-}
