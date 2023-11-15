2daa4df30b62ae4f222e55ad1de1abb8cf8b8a6d
zenbox <michael@zenbox.de>
2023-11-15 16:06:53 +0100

    dark theme approach

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
     // - - -
 
     setBoxes = () => {
-        let boxes = document.querySelectorAll("body > *, body > * > *");
+        let boxes = document.querySelectorAll(
+            "body > *, body > * > *, fieldset *"
+        );
         console.dir(boxes);
         boxes.forEach((box) => {
             box.classList.add("dev-box");
         });
     };
 
-    setBoxes();
+    // setBoxes();
     // - - -
 });
