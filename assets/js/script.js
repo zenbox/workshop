d1eff3000cc1ff276fcbe1e7003230f17fcd9448
zenbox <michael@zenbox.de>
2023-11-15 13:09:07 +0100

    form styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // - - -
 
     setBoxes = () => {
-        let boxes = document.querySelectorAll("body > *, body > * > *");
+        let boxes = document.querySelectorAll("body > *, body > * > *, fieldset > *");
         console.dir(boxes);
         boxes.forEach((box) => {
             box.classList.add("dev-box");
