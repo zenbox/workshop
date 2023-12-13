dbfb18c8113e12914e2d8361e37461cd17df1031
zenbox <michael@zenbox.de>
2023-12-13 08:59:39 +0100

    update hamburger

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
-const hamburger = document.querySelector("#hamburger");
+/**
+ * @desc Das Verhalten für das Hamburger Menu
+ *       und die Off Canvas Navigation
+ */
+document.addEventListener("DOMContentLoaded", () => {
+    // - - - - - - - - -
+    // Do something
+    const hamburger = document.querySelector("#hamburger");
 
-hamburger.addEventListener("click", (event) => {
-    hamburger.classList.toggle("hamburger--open");
-    document.body.classList.toggle("template-2--open");
+    hamburger.addEventListener("click", (event) => {
+        /* Toggle mobile menu 
+        "open" wird sowohl den <body> mitgegeben, als auch dem "#hamburger"
+        So lässt es sich in CSS einfach und lesbar gestalten.
+        */
+        hamburger.classList.toggle("hamburger--open");
+        document.body.classList.toggle("template-2--open");
+    });
+    // - - - - - - - - -
 });
