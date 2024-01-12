9074db1cd4edbf722df90a2d2572637a5f7faaac
zenbox <michael@zenbox.de>
2024-01-12 06:34:10 +0100

    update dump

--- src/model/dump.mjs
+++ src/model/dump.mjs
@@ -1,6 +1,8 @@
 import dbms from "mysql";
 import dbConfig from "./dbConfig.mjs";
 
+let i = 1;
+
 // Params: host, user, password [, port, database]
 let db = dbms.createConnection(dbConfig),
     sql = null;
@@ -9,11 +11,29 @@ db.on("error", function (error) {
     console.log(error);
 });
 
-db.query("CREATE DATABASE IF NOT EXISTS animals;");
-db.query("USE animals;");
+db.query("CREATE DATABASE IF NOT EXISTS animals;", (error, result) => {
+    if (error) {
+        console.log(error);
+        return false;
+    }
+    console.log(`${i++}. Datenbank angelegt.`);
+});
+db.query("USE animals;", (error, result) => {
+    if (error) {
+        console.log(error);
+        return false;
+    }
+    console.log(`${i++}. Datenbank ausgewählt.`);
+});
 
 // Tabelle anlegen
-db.query("DROP TABLE IF EXISTS sheeps;");
+db.query("DROP TABLE IF EXISTS sheeps;", (error, result) => {
+    if (error) {
+        console.log(error);
+        return false;
+    }
+    console.log(`${i++}. Tabelle gelöscht.`);
+});
 
 sql = `
     CREATE TABLE sheeps (
@@ -22,7 +42,13 @@ sql = `
     PRIMARY KEY (id) );
     `;
 
-db.query(sql);
+db.query(sql, (error, result) => {
+    if (error) {
+        console.log(error);
+        return false;
+    }
+    console.log(`${i++}. Tabelle angelegt.`);
+});
 
 sql = `
     INSERT INTO sheeps 
@@ -34,8 +60,13 @@ sql = `
         ('Schmuckschaf'),
         ('Haarschaf');
 `;
-db.query(sql, function () {
-    console.log("Datensätze geschrieben.");
+db.query(sql, function (error, result) {
+    if (error) {
+        console.log(error);
+        return false;
+    }
+    console.log(`${i++}. Datensätze geschrieben.`);
+    return true;
 });
 
 db.end();
